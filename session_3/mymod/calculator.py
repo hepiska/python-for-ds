cb0a593a51bdbbbcf419bf273f1a045c92628e54
ego <ego1403@gmail.com>
2021-08-31 21:40:23 +0700

    sesion 3 done

---
+++ session_3/mymod/calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def add(*args):
+    res = 0
+    for arg in args:
+        res += arg
+    return res
+
+def multiply(*args):
+    res = 1
+    for arg in args:
+        res = res * arg
+    return res
+
+def devide(*args):
+    [res, *sisa] = args
+    for arg in sisa:
+        res = res / arg
+    return res
+
+
+# In[ ]:
+
+
+
+
---
+++ session_3/runner.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+
+arr = np.array([2, 3, 4])
+
+print(arr)
